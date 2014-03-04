a593fb9c6801a88c6a8f76094f27c02f10204ed7
James Shore <jshore@jamesshore.com>
2014-03-04 13:24:54 -0800

    Implemented support for comparing flat types

--- src/_index_test.js
+++ src/_index_test.js
@@ -2,11 +2,52 @@
 "use strict";
 
 var expect = require("expect.js");
+var diff = require("./index.js");
 
-describe("nothing", function() {
+describe("flat types:", function() {
 
-	it("should do something", function() {
-		expect(1).to.eql(2);
+	it("undefined", function() {
+		expect(diff.match(undefined, undefined)).to.be(true);
+		expect(diff.match(undefined, null)).to.be(false);
+	});
+
+	it("null", function() {
+		expect(diff.match(null, null)).to.be(true);
+		expect(diff.match(null, undefined)).to.be(false);
+	});
+
+	it("boolean", function() {
+		expect(diff.match(true, true)).to.be(true);
+		expect(diff.match(false, false)).to.be(true);
+
+		expect(diff.match(false, true)).to.be(false);
+		expect(diff.match(true, false)).to.be(false);
+
+		expect(diff.match(true, 1)).to.be(false);
+	});
+
+	it("string", function() {
+		expect(diff.match("", "")).to.be(true);
+		expect(diff.match("foo", "foo")).to.be(true);
+
+		expect(diff.match("foo", "foo ")).to.be(false);
+	});
+
+	it("number", function() {
+		expect(diff.match(0, 0)).to.be(true);
+		expect(diff.match(NaN, NaN)).to.be(true);
+
+		expect(diff.match(0, 1)).to.be(false);
+		expect(diff.match(0, NaN)).to.be(false);
+		expect(diff.match(NaN, 0)).to.be(false);
+	});
+
+	it("function", function() {
+		expect(diff.match(a, a)).to.be(true);
+		expect(diff.match(a, b)).to.be(false);
+
+		function a() {}
+		function b() {}
 	});
 
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 "use strict";
 
+exports.match = function(a, b) {
+	if (typeof a === "number" && isNaN(a)) return isNaN(b);
+
+	return a === b;
+};
